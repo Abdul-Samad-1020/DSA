0c696bbb3fc71e70cf44c84c44639165f1fbb265
Abdul Samad <abdulsamadmughal1020@gmail.com>
2024-10-13 23:58:36 +0500

    Add files via upload

---
+++ Kadane's-Algorithm.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int n =5;
+	int arr[n] = { 1,2,3,4,5 };
+	int Currentsum =0;
+	int MAxsum = INT_MIN;
+	for (int i =0; i<n; i++){
+		Currentsum += arr[i];
+		MAxsum = max(Currentsum ,MAxsum);
+		if(Currentsum<0){
+			Currentsum=0;
+		}
+	}	
+		
+}
